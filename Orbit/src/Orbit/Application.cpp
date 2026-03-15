37cd51a6dd6d29cc77634ab5eced2cc27644de3c
Raffael <fuchs.raffael@hotmail.com>
2026-03-15 20:15:53 +0100

    made color

--- Orbit/src/Orbit/Application.cpp
+++ Orbit/src/Orbit/Application.cpp
@@ -6,6 +6,8 @@
 #include "Log.h"
 //my application 
 
+#include <GLFW/glfw3.h>
+
 namespace Orbit
 {
 	Application::Application()
@@ -22,6 +24,8 @@ namespace Orbit
 	{
 		while (m_Running)
 		{
+			glClearColor(0.4f, 0.6f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
