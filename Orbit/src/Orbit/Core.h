c14db207e711073d8894d7d10fab12090fedb329
Raffael <fuchs.raffael@hotmail.com>
2026-03-15 19:35:42 +0100

    made window class in detail

--- Orbit/src/Orbit/Core.h
+++ Orbit/src/Orbit/Core.h
@@ -10,4 +10,12 @@
 	#error Orbit only supports Windows!
 #endif
 
+#ifdef  OB_ENABLE_ASSERTS
+	#define OB_ASSERT(x, ...) { if(!(x)) { OB_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define OB_CORE_ASSERT(x, ...) { if(!(x)) { OB_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define OB_ASSERT(x, ...)
+	#define OB_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
